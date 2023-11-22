0cfe115d985bdd6fec6d8d3cd73573a59428d9ca
yutaro okuda <yutaro.okuda.76@gmail.com>
2023-11-22 20:52:27 +0800

    added form

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-
-echo 'hello';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice</title>
+</head>
+<body>
+    <form method="POST" action="<?php print($SERVER['PHP_SELF']) ?>">
+        <input type="text" name="formula"><br>
+        <input type="submit" name="btn" value="計算">
+    </form> 
+    <?php
+    $formula = $_POST['formula'];
+    echo($formula);
+    ?>
+    
+</body>
+</html>
