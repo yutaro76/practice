a26cf433662b6f6a77946f20fc67da9e68f0a2fc
yutaro okuda <yutaro.okuda.76@gmail.com>
2023-11-24 08:49:55 +0800

    set input after sent, get data from db

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php 
+$dsn = 'mysql:dbname=cake;host=localhost';
+$user = 'okuda';
+$pass = '';
+$pdo = new PDO($dsn, $user, $password);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,13 +16,39 @@
 </head>
 <body>
     <form method="POST" action="<?php print($SERVER['PHP_SELF']) ?>">
-        <input type="text" name="formula"><br>
+        <?php if (isset($_POST['formula'])) { ?>
+            <input type="text" name="formula" value="<?php echo($_POST['formula']); ?>"><br>
+        <?php } else {?>
+            <input type="text" name="formula"><br>
+        <?php } ?>
         <input type="submit" name="btn" value="計算">
     </form> 
     <?php
     $formula = $_POST['formula'];
     echo($formula);
     ?>
+
+    <?php 
+        try {
+            
+
+            $pstmt = $pdo->prepare('SELECT * FROM calc');
+            $pstmt->execute();
+            $result = $pstmt -> fetchAll();
+            echo '<pre>'; var_dump($result);
+
+            $pstmt = null;
+            $pdo = null;
+
+        } catch (PDOException $e) {
+            print "エラー!: " . $e->getMessage() . "<br/gt;";
+            die();
+        }
+
+    ?>
     
 </body>
 </html>
+
+<!-- PDO -->
+<!-- https://engineer-milione.com/programming/php-pdo.html -->
