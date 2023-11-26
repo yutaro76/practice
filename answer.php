270fc83cd238d17a0a724b7f028eb33b63710a8b
yutaro okuda <yutaro.okuda.76@gmail.com>
2023-11-26 10:24:50 +0800

    use two files

---
+++ answer.php
@@ -0,0 +1,59 @@
+<?php 
+$dsn = 'mysql:dbname=cake;host=localhost';
+$user = 'okuda';
+$pass = '';
+$pdo = new PDO($dsn, $user, $password);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice</title>
+</head>
+<body>
+    <a href="index.php">戻る</a>
+    <form method="POST" action="<?php print($SERVER['PHP_SELF']) ?>">
+        <input type="text" name="formula"><br>
+        <input type="submit" name="btn" value="計算" disabled>
+    </form> 
+
+    <?php 
+        try {
+            $formula = htmlspecialchars($_POST['formula']);
+            $pdo->exec('INSERT INTO calc SET formula="'.$formula.'"');
+            // $result = eval($formula);
+            // echo "$result";
+            
+        } catch (PDOException $e){
+            echo 'DB接続エラー:'. $e->getMessage();
+        }
+
+
+        //     $pstmt = $pdo->prepare('SELECT * FROM calc');
+        //     $pstmt->execute();
+        //     $result = $pstmt -> fetchAll();
+
+        //     $pstmt = null;
+        //     $pdo = null;
+
+        // } catch (PDOException $e) {
+        //     print "エラー!: " . $e->getMessage() . "<br/gt;";
+        //     die();
+        // }
+
+    ?>
+
+
+<!-- 
+    <?php
+    $formula = $_POST['formula'];
+    echo($formula);
+    ?> -->
+
+    
+    
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,54 +1,14 @@
-<?php 
-$dsn = 'mysql:dbname=cake;host=localhost';
-$user = 'okuda';
-$pass = '';
-$pdo = new PDO($dsn, $user, $password);
-
-?>
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Practice</title>
 </head>
 <body>
-    <form method="POST" action="<?php print($SERVER['PHP_SELF']) ?>">
-        <?php if (isset($_POST['formula'])) { ?>
-            <input type="text" name="formula" value="<?php echo($_POST['formula']); ?>"><br>
-        <?php } else {?>
-            <input type="text" name="formula"><br>
-        <?php } ?>
+    <form method="POST" action="answer.php">
+        <input type="text" name="formula"><br>
         <input type="submit" name="btn" value="計算">
-    </form> 
-    <?php
-    $formula = $_POST['formula'];
-    echo($formula);
-    ?>
-
-    <?php 
-        try {
-            
-
-            $pstmt = $pdo->prepare('SELECT * FROM calc');
-            $pstmt->execute();
-            $result = $pstmt -> fetchAll();
-            echo '<pre>'; var_dump($result);
-
-            $pstmt = null;
-            $pdo = null;
-
-        } catch (PDOException $e) {
-            print "エラー!: " . $e->getMessage() . "<br/gt;";
-            die();
-        }
-
-    ?>
-    
+    </form>         
 </body>
 </html>
-
-<!-- PDO -->
-<!-- https://engineer-milione.com/programming/php-pdo.html -->
